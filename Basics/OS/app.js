6f6a432da03c108484a0ec54806a89f47138adc0
mikaelbeat <ryynanenphm@gmail.com>
2019-03-08 13:39:42 +0200

    Update

---
+++ Basics/OS/app.js
@@ -0,0 +1,19 @@
+
+const os = require("os");
+
+var totalMemory = os.totalmem();
+var freeMemory = os.freemem();
+
+console.log("\n");
+console.log("Total memory: " + totalMemory);
+console.log("Free memory: " + freeMemory);
+
+console.log("\n*********************************\n");
+
+console.log(`Total memory: ${totalMemory}`);
+console.log(`Free memory: ${freeMemory}`);
+console.log("\n");
+
+
+
+
