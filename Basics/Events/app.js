5a66cfd271de2f7106bcdb64108321633aaa4fdc
mikaelbeat <ryynanenphm@gmail.com>
2019-03-08 14:30:21 +0200

    Update

--- Basics/Events/app.js
+++ Basics/Events/app.js
@@ -3,9 +3,19 @@ const EventEmitter = require('events');
 
 const emitter = new EventEmitter();
 
-emitter.on('messageLogged', function(){
-    console.log('Listener called');
+// Register listener
+emitter.on('messageLogged', (event) => {
+    console.log('Listener called', event);
 });
 
-emitter.emit('messageLogged');
+// Raise an event
+emitter.emit('messageLogged', {id: 1, url: 'http://'});
+
+// Raise: logging (data: message)
+emitter.on('logging', (event) => {
+    console.log('Listener called for logging', event);
+});
+
+emitter.emit('logging', {id: 1, url: 'http://homepage.com'});
+
 
